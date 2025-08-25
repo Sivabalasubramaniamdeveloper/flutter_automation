29140175d285ec844c898ba16c1c37615dae4c4d
Sivabalasubramaniamdeveloper <sivabala.piramuthu@gmail.com>
2025-08-25 11:18:18 +0530

    "IOS with flavour setup"

--- lib/config/flavor/flavor_config.dart
+++ lib/config/flavor/flavor_config.dart
@@ -1,3 +1,40 @@
+enum Flavor {
+  sit, uat, prod
+}
+
 class FlavorConfig {
-  static bool get isDevelopment => true;
+  static Flavor? appFlavor;
+
+  static String get title {
+    switch (appFlavor) {
+ case Flavor.sit:
+      return 'SIVA SIT'; case Flavor.uat:
+      return 'SIVA UAT'; case Flavor.prod:
+      return 'SIVA PROD';
+      default:
+        return 'SIVA';
+    }
+  }
+
+  static bool get isDevelopment {
+    switch (appFlavor) {
+case Flavor.sit:
+        return true;case Flavor.uat:
+        return true;case Flavor.prod:
+        return false;
+      default:
+        return false;
+    }
+  }
+
+  static bool get isSIVA {
+    switch (appFlavor) {
+case Flavor.sit:
+      return true;case Flavor.uat:
+      return true;case Flavor.prod:
+      return true;
+      default:
+        return true;
+    }
+  }
 }
